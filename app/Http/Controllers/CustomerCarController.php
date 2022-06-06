d7b8352b837a7d1f665ae680d38ad3d8e5dc6ea5
Alrandis <Alrandisisdernii@gmail.com>
2022-06-06 20:58:17 +0700

    Создал пустые таблицы (модели)

---
+++ app/Http/Controllers/CustomerCarController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Customer_car;
+use Illuminate\Http\Request;
+
+class CustomerCarController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Customer_car  $customer_car
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Customer_car $customer_car)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Customer_car  $customer_car
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Customer_car $customer_car)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Customer_car  $customer_car
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Customer_car $customer_car)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Customer_car  $customer_car
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Customer_car $customer_car)
+    {
+        //
+    }
+}
---
+++ app/Http/Controllers/ServiceCategoriesController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Service_Categories;
+use Illuminate\Http\Request;
+
+class ServiceCategoriesController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Service_Categories  $service_Categories
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Service_Categories $service_Categories)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Service_Categories  $service_Categories
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Service_Categories $service_Categories)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Service_Categories  $service_Categories
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Service_Categories $service_Categories)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Service_Categories  $service_Categories
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Service_Categories $service_Categories)
+    {
+        //
+    }
+}
